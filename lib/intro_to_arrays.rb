e20447d263e015e59f8eae32e8929c42839867e5
Janice Pena <jipena94@gmail.com>
2019-04-08 00:59:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,12 @@
+def instantiate_new_array
+  @my_new_array = instantiate_new_array
+end
+
+def instantiate_new_array
+  []
+end
+
+def arrat_with_two_elements
+  instantiate_new_array ["Janice", "John", "Baby"]
+end
+
