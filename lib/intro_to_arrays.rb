1d03925b8f2121a5c9b284df92577d820f1ec69e
Janice Pena <jipena94@gmail.com>
2019-04-08 01:20:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,7 +8,8 @@ def array_with_two_elements
 end
 
 def first_element(my_first_element)
-  famous_cats.index("John"
+   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   my_first_element.first
 end
 
 def third_element.index
