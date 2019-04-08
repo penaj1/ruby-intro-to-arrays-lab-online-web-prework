cb273201caaceb385062c4e44a9ed6b05c1d02cc
Janice Pena <jipena94@gmail.com>
2019-04-08 01:22:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -13,7 +13,8 @@ def first_element(my_first_element)
 end
 
 def third_element(my_third_element)
-  
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   my_third_element.first
 end
 
 def last_element
