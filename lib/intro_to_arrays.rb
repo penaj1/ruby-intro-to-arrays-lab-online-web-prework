6a2e070e6d11bf3b71587a26249374c56d438686
Janice Pena <jipena94@gmail.com>
2019-04-08 01:30:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -33,5 +33,6 @@ def last_element_with_array_methods(last_country)
 end
 
 def length_of_array
+  @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
 end
 
